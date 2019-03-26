c6f8b19eb1f7467b973e4cea62f5e438cb98b175
Himanshu Jain <jainhimanshu2047@gmail.com>
2019-03-26 12:50:33 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req,res){
+   res.render("home.ejs"); 
+});
+
+app.get("/login", function(req,res){
+    res.render("signup.ejs");
+})
+
+app.listen(process.env.PORT, process.env.IP,function(){
+    console.log("Server is running!!")
+});
