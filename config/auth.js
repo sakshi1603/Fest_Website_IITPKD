03864b3096bb55dcecda724ed7b95ac4c804654f
Himanshu Jain <jainhimanshu2047@gmail.com>
2019-07-02 11:12:11 +0530

    Bug Fixes deploy

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
 	'googleAuth' : {
 		'clientID': "183527246547-0kk29ge62um59ou2919fk3lmqiv6nbt0.apps.googleusercontent.com",
 		'clientSecret': 'WYO1SoBP1VJjZ9NUYTDePEoT',
-		'callbackURL': "https://flannel-worms-19088.herokuapp.com/auth/google/callback"
+		'callbackURL': "www.petrichor-ca-portal.herokuapp.com/auth/google/callback"
 	}
 }
